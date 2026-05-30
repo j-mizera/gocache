5e7d5224985688d77f68b540f335262d8a13ef5d
witherxse <k.cstmz@gmail.com>
2026-05-30 17:42:11 +0000

    bench: add observability attribution benchmarks
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ api/events/events_bench_test.go
@@ -0,0 +1,44 @@
+package events
+
+import "testing"
+
+var benchmarkMetadata = map[string]string{
+	"shared.rex.traceparent": "00-abc-def-01",
+	"_command":               "SET",
+	"_arg_count":             "2",
+}
+
+func BenchmarkEventConstructors(b *testing.B) {
+	args := []string{"key", "value"}
+	ctx := map[string]string{
+		"_command":               "SET",
+		"_arg_count":             "2",
+		"shared.rex.traceparent": "00-abc-def-01",
+	}
+
+	b.Run("command_started", func(b *testing.B) {
+		for b.Loop() {
+			_ = NewCommandStarted("SET", args, benchmarkMetadata).WithOperationID("cmd_1")
+		}
+	})
+	b.Run("command_completed", func(b *testing.B) {
+		for b.Loop() {
+			_ = NewCommandCompleted("SET", args, 42_000, "OK", "", benchmarkMetadata).WithOperationID("cmd_1")
+		}
+	})
+	b.Run("operation_started", func(b *testing.B) {
+		for b.Loop() {
+			_ = NewOperationStarted("cmd_1", "command", "conn_1", ctx)
+		}
+	})
+	b.Run("operation_completed", func(b *testing.B) {
+		for b.Loop() {
+			_ = NewOperationCompleted("cmd_1", "command", 42_000, "completed", "", ctx)
+		}
+	})
+	b.Run("log_entry", func(b *testing.B) {
+		for b.Loop() {
+			_ = NewLogEntry("info", "command completed", "", ctx).WithOperationID("cmd_1")
+		}
+	})
+}
---
+++ pkg/logcollector/collector_bench_test.go
@@ -0,0 +1,38 @@
+package logcollector
+
+import (
+	"strings"
+	"testing"
+)
+
+func BenchmarkCollectorParseLine(b *testing.B) {
+	jsonLine := []byte(`{"level":"info","source":"server","message":"command completed","_operation_id":"cmd_1","_ctx":{"_command":"SET","_arg_count":"2","shared.rex.traceparent":"00-abc-def-01"},"elapsed_ms":1}`)
+	plainLine := []byte("plain plugin output")
+
+	b.Run("json", func(b *testing.B) {
+		em := &mockEmitter{}
+		c := New(em)
+		b.ResetTimer()
+		for b.Loop() {
+			c.parseLine("server", jsonLine)
+		}
+	})
+	b.Run("plain", func(b *testing.B) {
+		em := &mockEmitter{}
+		c := New(em)
+		b.ResetTimer()
+		for b.Loop() {
+			c.parseLine("plugin", plainLine)
+		}
+	})
+	b.Run("source_reader", func(b *testing.B) {
+		line := string(jsonLine) + "\n"
+		b.ResetTimer()
+		for b.Loop() {
+			em := &mockEmitter{}
+			c := New(em)
+			c.AddSource("server", strings.NewReader(line))
+			c.Wait()
+		}
+	})
+}
